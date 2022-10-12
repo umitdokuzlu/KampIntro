e215c20d7e4f275e77282e4939d43007abdf042d
umitdokuzlu <umut-dokuzlu@hotmail.com>
2022-10-12 19:10:08 +0300

    C# Başlangıç

---
+++ OOP1/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace OOP1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Product product1 = new Product();
+            product1.Id = 1;
+            product1.CategoryId = 2;
+            product1.ProductName = "Masa";
+            product1.UnitPrice = 500;
+            product1.UnitInStock = 3;
+
+            Product product2 = new Product { Id = 2,CategoryId=5,UnitInStock=5,ProductName="Kalem",UnitPrice=35 };
+
+            ProductManager productManager = new ProductManager();
+            productManager.Add(product1);
+
+            productManager.Topla2(3, 6);
+
+            int toplamaSonucu = productManager.Topla(3, 6);
+            Console.WriteLine(toplamaSonucu*2);
+        }
+    }
+}
