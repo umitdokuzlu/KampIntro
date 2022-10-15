42f5ab01ab26c1a7f43467f5614dafa13808f5d8
umitdokuzlu <umut-dokuzlu@hotmail.com>
2022-10-15 12:48:01 +0300

    OOP

---
+++ OOP2/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace OOP2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            GercekMusteri musteri1 = new GercekMusteri();
+            musteri1.Id = 1;
+            musteri1.MusteriNo = "12345";
+            musteri1.Adi = "Engin";
+            musteri1.Soyadi = "Demiroğ";
+            musteri1.TcNo = "12345678910";
+
+            TuzelMuseteri musteri2 = new TuzelMuseteri();
+            musteri2.Id = 2;
+            musteri2.MusteriNo = "54321";
+            musteri2.SirketAdi = "Kodlama.io";
+            musteri2.VergiNo = "1234567890";
+
+            Musteri musteri3 = new GercekMusteri();
+            Musteri musteri4 = new TuzelMuseteri();
+
+            MusteriManager musteriManager= new MusteriManager();
+            musteriManager.Ekle(musteri1);
+            musteriManager.Ekle(musteri1);
+        }
+    }
+}
---
+++ OOP3/BasvuruManager.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OOP3
+{
+    internal class BasvuruManager
+    {
+        public void BasvuruYap(IKrediManager krediManager,ILoggerService loggerService)
+        {
+
+            
+            krediManager.Hesapla();
+            loggerService.Log();
+        }
+
+        public void KrediOnBilgilendirmesiYap(List<IKrediManager> Krediler)
+        {
+            foreach (IKrediManager kredi in Krediler)
+            {
+                kredi.Hesapla();
+            }
+        }
+    }
+}
